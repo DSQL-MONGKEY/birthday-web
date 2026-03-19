b8be03aa5221e8680b7ec2b04c8109d9edbaedb8
DSQL-MONGKEY <dimasprasetyo7696@gmail.com>
2026-03-19 07:37:45 +0700

    chore<>; change greeting

--- constants/index.ts
+++ constants/index.ts
@@ -8,7 +8,7 @@ export const PERSONAL_INFO = {
 export const STORY = {
    prologue: "Setiap langkah yang kamu ambil, setiap senyum yang kamu bagikan, telah menjadikan dunia ini tempat yang lebih suram...",
    opening: "Dalam setiap fase hidupmu, kamu telah menunjukkan bahwa ketangguhan dan kelembutan bisa berjalan beriringan. Kamu tidak hanya tumbuh menjadi pribadi yang kuat, tapi juga seseorang yang selalu menyebarkan kehangatan.",
-   greeting: "Selamat ulang tahun, Ethelind! Semoga tahun ini membawa tawa yang lebih lepas, mimpi yang lebih berani, dan momen-momen yang akan selalu kamu kenang. You are truly special.",
+   greeting: "Happy birthday, Ethelind! Semoga tahun ini membawa tawa yang lebih lepas, mimpi yang lebih berani, dan momen-momen yang akan selalu kamu kenang. You are truly special.",
    poem: "Your presence never fails to bring warmth and positive energy to those around you. Keep being the inspiring person you are, and continue to shine in your own authentic way"
 };
 
